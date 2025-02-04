ecce13e6dc9fc6ab10ab9fdf7e54fe54f4f05804
nvakhovska <nataliia.vakhovska@gmail.com>
2025-02-04 14:32:20 -0500

    implented additional error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,66 @@
+import AppError from "../utils/appError.js";
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFilds = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  const message = `Duplicate field value: ${value}. Please use another value.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.error).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR", err);
+
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
 export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV == "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV == "production") {
+    let error = { ...err };
+
+    if (error.name === "CastError") {
+      error = handleCastErrorDB(error);
+    }
+    if (error.code === 11000) {
+      error = handleDuplicateFilds(error);
+    }
+    if (error.name === "ValidationError") {
+      error = handleValidationErrorDB(error);
+    }
+    sendErrorProd(error, res);
+  }
 };
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import app from "./app.js";
 
+//for safety
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION!");
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
@@ -18,6 +25,15 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+//for safety
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION!");
+  console.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
